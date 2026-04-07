782d0e2454de08fbb3ba7f5fe1678028c3051bbf
pmaasz <PhilipMaasz@aol.com>
2026-04-07 22:08:41 +0200

    Bump version to 0.1.1 for bugfix release

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ def read_file(filename):
 
 setup(
     name='openlens',
-    version='0.1.0',
+    version='0.1.1',
     description='An interactive optical lens design and simulation tool with ISO 10110 export',
     long_description=read_file('README.md'),
     long_description_content_type='text/markdown',
